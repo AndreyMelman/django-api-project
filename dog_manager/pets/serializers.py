81eabf7ff39c8385530e6a4d80dc81c0e794c4e6
Andrei Lahunovich <lagunmelman@gmail.com>
2025-04-02 12:54:58 +0300

    Добавил dock strings для сериализатора

--- dog_manager/pets/serializers.py
+++ dog_manager/pets/serializers.py
@@ -3,6 +3,20 @@ from .models import Dog, Breed
 
 
 class BreedSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Breed.
+
+    Fields:
+        id (int): ID породы
+        name (str): Название породы
+        size (str): Размер собаки
+        friendliness (int): Дружелюбие собаки
+        trainability (int): Обучаемость собаки
+        shedding_amount (int): Количество линьки
+        exercise_needs (int): Потребность в физической активности
+        dogs_count (int): Количество собак данной породы
+    """
+
     dogs_count = serializers.IntegerField(read_only=True)
 
     class Meta:
@@ -11,6 +25,15 @@ class BreedSerializer(serializers.ModelSerializer):
 
 
 class DogListSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Dog.
+
+    Fields:
+        id (int): ID собаки
+        name (str): Имя собаки
+        age (int): Возраст собаки
+    """
+
     avg_breed_age = serializers.FloatField(read_only=True)
 
     class Meta:
@@ -19,6 +42,15 @@ class DogListSerializer(serializers.ModelSerializer):
 
 
 class DogDetailSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Dog.
+
+    Fields:
+        id (int): ID собаки
+        name (str): Имя собаки
+        age (int): Возраст собаки
+    """
+
     same_breed_count = serializers.IntegerField(read_only=True)
 
     class Meta:
